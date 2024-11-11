9fa5f590d0104eae5a6e1ef3851ef8235e0fa75e
colincano20 <colincano05@gmail.com>
2024-11-11 11:25:10 -0600

    Added files
    Added midterms for calc and COMP SCi. mw homework and LIT quizzes. Also QOTD and rough draft

---
+++ 2024-25/1stSemester/ComputerScience1/QOTD/qotd20.py
@@ -0,0 +1,62 @@
+# CS1210: QotD20
+######################################################################
+# Complete the signed() function, certifying that:
+#  1) the code below is entirely your own work, and
+#  2) you have not shared it with anyone else.
+#
+# ToDo: Change the word "hawkid" between the two double quote marks to
+# match your own hawkid. Your hawkid is the "login identifier" (not
+# your email address) that you use to login to all University
+# services.
+#
+def signed():
+    return(["cmcano"])
+
+######################################################################
+# Roman numerals are strings of characters that are read from left to
+# right. Each character represents a specific value:
+#    M=1000, D=500, C=100, L=50, X=10, V=5 and I=1
+# If sorted from larger to smaller, a string of these would then
+# be interpreted as the sum of those elements. So:
+#    MDCLXXXV = 1000+500+100+50+10+10+10+5 = 1685
+# In addition, two character combinations arranged from smaller to
+# larger are also allowed. Thus we can use IV rather than IIII to
+# represent the number 4, and MCM to represent 1900.
+#
+# Specification: rom2int(R) takes a string, R, representing a legal
+# Roman numeral, and returns the equivalent integer.
+#
+# A Roman numeral is legal if it is generally arranged from largest
+# value to smallest, with the exception of allowing a
+# single-small-large-letter combination at the end of the string of
+# "large" letters. Thus "XXIXV" is legal, but "XIXXV" is not.
+#
+# Hint: an iterative approach seems warranted, traversing R
+# left-to-right while accumulating is integer counterpart.
+#
+# Hint: please, no giant if/elif/else statements checking each Roman
+# numeral character; you know better than that by now!
+#
+# Here, you can assume that the input R is a legal Roman number; in
+# other words, if R is illegal, any answer is correct.
+#
+# Example:
+#   >>> rom2int("MCCX")
+#   1210
+#   >>> rom2int("MCMXCIV")
+#   1994
+#
+def rom2int(R):
+    Dict = { 'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1 } #dictionary
+    total = 0
+    previous = 0 
+    for i in R: #loop through string
+        if Dict[i] > previous:
+            total += Dict[i] - 2 *previous   #subtracts if index is bigger then previous
+        else:
+            total += Dict[i] #just straight adds to total
+        previous = Dict[i] #marks index as previous
+    return total
+
+print(rom2int("MCCX"))
+print(rom2int("MCMXCIV"))
---
+++ 2024-25/1stSemester/ComputerScience1/QOTD/qotd21.py
@@ -0,0 +1,64 @@
+# CS1210: QotD21
+######################################################################
+# Complete the signed() function, certifying that:
+#  1) the code below is entirely your own work, and
+#  2) you have not shared it with anyone else.
+#
+# ToDo: Change the word "hawkid" between the two double quote marks to
+# match your own hawkid. Your hawkid is the "login identifier" (not
+# your email address) that you use to login to all University
+# services.
+#
+def signed():
+    return(["cmcano"])
+
+######################################################################
+# Write the recursive solution for rom2int().
+#
+# Roman numerals are strings of characters that are read from left to
+# right. Each character represents a specific value:
+#    M=1000, D=500, C=100, L=50, X=10, V=5 and I=1
+# If sorted from larger to smaller, a string of these would then
+# be interpreted as the sum of those elements. So:
+#    MDCLXXXV = 1000+500+100+50+10+10+10+5 = 1685
+# In addition, two character combinations arranged from smaller to
+# larger are also allowed. Thus we can use IV rather than IIII to
+# represent the number 4, and MCM to represent 1900.
+#
+# Specification: rom2int(R) takes a string, R, representing a legal
+# Roman numeral, and returns the equivalent integer.
+#
+# A Roman numeral is legal if it is generally arranged from largest
+# value to smallest, with the exception of allowing a
+# single-small-large-letter combination at the end of the string of
+# "large" letters. Thus "XXIXV" is legal, but "XIXXV" is not.
+#
+# Here, you can assume that the input R is a legal Roman number; in
+# other words, if R is illegal, any answer is correct.
+#
+# Example:
+#   >>> rom2int("MCCX")
+#   1210
+#   >>> rom2int("MCMXCIV")
+#   1994
+#
+values = {'M':1000, 'CM':900, 'D':500, 'C':100, 'XC':90, 'L':50, 'X':10, 'IX':9, 'V':5, 'IV':4, 'I':1}
+#
+def rom2int(R):
+    global values
+    if len(R) == 0: 
+        return 0 
+    if len(R) > 1 and R[:2] in values: 
+        return values[R[:2]] + rom2int(R[2:]) 
+    else: 
+        return values[R[0]] + rom2int(R[1:])
+
+
+print(rom2int("MCCX"))
+print(rom2int("MCMXCIV"))
+     
+
+
+         
+
+
